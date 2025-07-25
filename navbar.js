487830912a4f67f23fbb38555608e4b6f412f235
Francisco Pérez <drfperez@gmail.com>
2025-07-25 17:50:19 +0200

    Update navbar.js

--- navbar.js
+++ navbar.js
@@ -1,6 +1,6 @@
 // navbar.js
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('navbar.html')
+    fetch('https://drfperez.github.io/navbar.html')
         .then(response => response.text())
         .then(data => {
             const placeholder = document.getElementById('navbar-placeholder');
